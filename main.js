7cb0eadedba537a3053f60f2c858e76eb6cc20a4
ejliu68 <ejliu@mit.edu>
2017-07-11 13:00:24 -0400

    Created basic shell for demo

---
+++ main.js
@@ -0,0 +1,21 @@
+var electron = require('electron');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+
+//Global reference of window object
+var mainWindow = null;
+
+app.on('window-all-closed', function(){
+  if (process.platform != 'darwin'){
+    app.quit();
+  }
+});
+
+app.on('ready', function(){
+  // Create browser window
+  mainWindow = new BrowserWindow({width:300,height:300});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function(){
+    mainWindow = null;
+  });
+});
